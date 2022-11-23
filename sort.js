5e3b3c8534f0ca32c367b4c10658eaac18a31cdd
Pavlik1996 <miki2012miki@gmail.com>
2022-11-23 10:59:01 +0300

    delete

--- sort.js
+++
@@ -1,9 +0,0 @@
-// function compareNumeric(a, b) {
-//   if (a > b) return 1;
-//   if (a == b) return 0;
-//   if (a < b) return -1;
-// }
-
-let arr = [1, 2, 15, 4, 77, 4, 4, 28, 3333, 21838912];
-
-console.log(arr.sort((a, b) => a - b));
