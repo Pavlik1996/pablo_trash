6940280ada43eeb5d853cd4adb6211828834e6ff
Pavlik1996 <miki2012miki@gmail.com>
2022-11-24 15:07:12 +0300

    add str

---
+++ str${}.js
@@ -0,0 +1,9 @@
+function animal(obj) {
+  return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs.";
+}
+
+console.log(animal({ name: "dog", legs: 4, color: "white" }));
+
+function animal(obj) {
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
